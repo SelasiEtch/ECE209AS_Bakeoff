1ab9ad047c5ee52f9a68e780da0f184924714dca
emagana6622 <123432248+emagana6622@users.noreply.github.com>
2023-02-03 19:04:11 -0800

    Update ClassifyVibration.java

--- ClassifyVibration.java
+++ ClassifyVibration.java
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,30 +13,31 @@ import processing.sound.Sound;
 import processing.sound.SoundFile;
 import processing.sound.Waveform;
 
+import weka.core.SerializationHelper;
+
 /* A class with the main function and Processing visualizations to run the demo */
 
 public class ClassifyVibration extends PApplet {
 
 	FFT fft;
-	FFT knuckle_fft, nail_fft, pad_fft,silence_fft;
 	AudioIn in;
 	Waveform waveform;
 	Waveform knuckle_wav, nail_wav, pad_wav,silence_wav;
 	int bands = 512;
 	int nsamples = 1024;
 	float[] spectrum = new float[bands];
-	float[] tracker_spec = new float[bands];
-	float[] tracker_spec2 = new float[bands];
-	float[] tracker_spec3 = new float[bands];
-	float[] mea = new float[bands];
 	float[] fftFeatures = new float[bands];
-	String[] classNames = {"knuckle","nail", "silence"};
+	String[] classNames = {"knuckle","nail", "silence","tap","slide"};
 	int temp = 0;
 	int classIndex = 0;
 	int dataCount = 0;
-	SoundFile knuckle, nail, pad,silence;
+	String temp_store;
 	MLClassifier classifier;
-	
+	boolean startFrame=false;
+	String[] framelabels = new String[100];
+	int numlabels=0;
+	SoundFile drum;
+	String classification;
 	Map<String, List<DataInstance>> trainingData = new HashMap<>();
 	{for (String className : classNames){
 		trainingData.put(className, new ArrayList<DataInstance>());
@@ -43,29 +47,8 @@ public class ClassifyVibration extends PApplet {
 		DataInstance res = new DataInstance();
 		res.label = label;
 		
-		
-		/*if(temp==1)
-		{
-			mea=tracker_spec.clone();
-			println("cloning knuckle");
-		}
-		else if(temp==2)
-		{
-			mea= tracker_spec2.clone();
-		}
-		else if(temp==4)
-		{
-			mea= tracker_spec3.clone();
-		}
-		else if(temp==3)
-		{
-			mea=fftFeatures.clone();
-		}*/
 		res.measurements = fftFeatures.clone();
-		/*if(mea==tracker_spec3)
-		{
-			println("success");
-		}*/
+		
 		return res;
 	}
 	
@@ -79,36 +62,8 @@ public class ClassifyVibration extends PApplet {
 
 	public void setup() {
 		
-		 knuckle = new SoundFile(this, "right_forefinger_knuckle_1-5inches_away.wav");
-		 nail = new SoundFile(this, "right_forefinger_nail_1-5inches_away.wav");
-		 silence = new SoundFile(this, "silence.wav");
-		 //pad = new SoundFile(this, "right_forefinger_pad_1-5inches_away.wav");
-		 knuckle_wav = new Waveform(this, nsamples);
-		 nail_wav = new Waveform(this, nsamples);
-		 silence_wav = new Waveform(this, nsamples);
-		 //pad_wav = new Waveform(this, nsamples);
-		 knuckle_fft = new FFT(this, bands);
-		 nail_fft = new FFT(this, bands);
-		 silence_fft = new FFT(this, bands);
-		 //pad_fft = new FFT(this, bands);
 		 
-		 
-		
-		// pad_wav.input(pad);
-		 //knuckle.play();
-		 
-		 //nail.play();
-			
-		 nail_wav.input(nail);
-		 nail_fft.input(nail);
-		 knuckle_wav.input(knuckle);
-			knuckle_fft.input(knuckle);	
-			silence_wav.input(silence);
-			silence_fft.input(silence);	
-			 
-		
-		 
-		
+		drum = new SoundFile(this,"582758__martina_leitschuh__moan_outdoors_man.wav");
 		/* list all audio devices */
 		Sound.list();
 		Sound s = new Sound(this);
@@ -159,28 +114,7 @@ public class ClassifyVibration extends PApplet {
 			line( i, height, i, height - spectrum[i]*height*40);
 			fftFeatures[i] = spectrum[i];
 		}
-		 /*if(temp==1)
-		 {
-					 	knuckle_fft.analyze(tracker_spec);
-					 	
-						trainingData.get("knuckle").add(captureInstance("knuckle"));
-				
-		 }
-		if (temp==2) {
-		
-		nail_fft.analyze(tracker_spec2);	
-		println(tracker_spec2);
-		trainingData.get("nail").add(captureInstance("nail"));
-		
-		}
-		if (temp==4) {
-			
-			silence_fft.analyze(tracker_spec3);	
-			//println(tracker_spec2);
-			trainingData.get("silence").add(captureInstance("silence"));
-			
-			}*/
-			
+	
 		
 			 
 			
@@ -196,8 +130,49 @@ public class ClassifyVibration extends PApplet {
 			// Yang: add code to stabilize your classification results
 			
 			text("classified as: " + guessedLabel, 20, 30);
-			println(guessedLabel);
-		}else {
+			if(guessedLabel.charAt(1)!='i' && startFrame==false)
+			{
+				startFrame=true;
+				framelabels[numlabels]=guessedLabel;
+				numlabels++;
+				
+			//println(guessedLabel);
+				
+			}
+			else if(guessedLabel.charAt(1)!='i' && startFrame==true)
+			{
+				framelabels[numlabels]=guessedLabel;
+				numlabels++;
+			}
+			else
+			{
+				startFrame=false;
+				if(numlabels>1)
+				{
+					classification = framelabels[0];
+					numlabels=0;
+					println(classification);
+					if(classification.charAt(0)=='t')
+					{
+						drum.play();
+					}
+					
+				}
+				else
+				{
+					classification = "silent";
+					numlabels=0;
+				}
+				
+			}
+			temp_store = guessedLabel;
+		}
+		/*else if(add_cl==true){
+			text(classNames[classIndex], 20, 30);
+			dataCount = trainingData.get(classNames[classIndex]).size();
+			text("Data collected: " + dataCount, 20, 60);
+		}*/
+		else {
 			text(classNames[classIndex], 20, 30);
 			dataCount = trainingData.get(classNames[classIndex]).size();
 			text("Data collected: " + dataCount, 20, 60);
@@ -212,10 +187,6 @@ public class ClassifyVibration extends PApplet {
 		}
 		
 		else if (key == 't') {
-			/*temp=3;
-			knuckle.stop();
-			 nail.stop();
-			 silence.stop();*/
 			if(classifier == null) {
 				println("Start training ...");
 				classifier = new MLClassifier();
@@ -224,40 +195,58 @@ public class ClassifyVibration extends PApplet {
 				classifier = null;
 			}
 		}
+		/*else if(key=='a')
+		{
+			if (add_cl==false)
+			{
+				add_cl=true;
+			}
+			else if(add_cl == true)
+			{
+				add_cl=false;
+				println("Start training ...");
+				//classifier = new MLClassifier();
+				classifier.train(trainingData);
+			}
+		}*/
 		
 		else if (key == 's') {
 			// Yang: add code to save your trained model for later use
+
+			
+			/*try {
+				ObjectOutputStream oos = new ObjectOutputStream(
+			            new FileOutputStream("C:\\Users\\emaga\\OneDrive\\Desktop\\training.model"));
+				oos.writeObject(classifier);
+				oos.flush();
+				oos.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			;*/
+			try {
+				weka.core.SerializationHelper.write("C:\\Users\\emaga\\OneDrive\\Desktop\\training.model", classifier);
+				System.out.println("Saving done!");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		else if (key == 'l') {
 			// Yang: add code to load your previously trained model
+			try {
+				 classifier = (MLClassifier) weka.core.SerializationHelper.read("C:\\Users\\emaga\\OneDrive\\Desktop\\training.model");
+				 System.out.println("Loading done!");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 			
-		
-		/*else if (key == 'k')
-		{
-			knuckle.play();
-			nail.stop();
-			silence.stop();
-			temp =1;
-			
-		}
-		else if (key == 'n')
-		{
-			
-			nail.play();
-			knuckle.stop();
-			silence.stop();
-			temp=2;
-			
-		}
-		else if(key =='m'){
-		
-			silence.play();
-			knuckle.stop();
-			nail.stop();
-			temp=4;
-	}*/
+	
 		else
 		{
 		 
